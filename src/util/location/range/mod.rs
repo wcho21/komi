baba46449fe169cc7c6cbbae4987cdbd84125e85
Cho <wcho.dev@gmail.com>
2025-05-12 14:25:11 +0000

    place better

--- src/util/location/range/mod.rs
+++ src/util/location/range/mod.rs
@@ -11,8 +11,13 @@ impl Range {
     pub const fn new(begin: Spot, end: Spot) -> Self {
         Range { begin, end }
     }
+
+    pub const fn from_spot(spot: &Spot) -> Self {
+        Range { begin: *spot, end: *spot }
+    }
 }
 
+#[deprecated]
 pub const fn from_spot(spot: &Spot) -> Range {
     Range::new(*spot, *spot)
 }
