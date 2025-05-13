98a6b892e0ba2269ee9ca4fcdea794320a69c67e
Cho <wcho.dev@gmail.com>
2025-05-13 10:30:59 +0000

    add binding powers

---
+++ src/core/syntax/bp/mod.rs
@@ -0,0 +1,24 @@
+/// Binding powers for an infix token.
+pub struct Bp {
+    pub left: u8,
+    pub right: u8,
+}
+
+static LOWEST_BP: Bp = Bp {
+    left: 0o0,
+    right: 0o1,
+};
+static SUMMATIVE_BP: Bp = Bp {
+    left: 0o30,
+    right: 0o31,
+};
+
+impl Bp {
+    pub fn get_summative() -> &'static Bp {
+        &SUMMATIVE_BP
+    }
+
+    pub fn get_lowest() -> &'static Bp {
+        &LOWEST_BP
+    }
+}
