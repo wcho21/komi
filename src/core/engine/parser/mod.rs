f4ab346a22067e6c8ded4bff3e91cc0d7f34545a
Cho <wcho.dev@gmail.com>
2025-05-13 10:32:55 +0000

    parse infix plus

--- src/core/engine/parser/mod.rs
+++ src/core/engine/parser/mod.rs
@@ -1,7 +1,7 @@
 mod token_scanner;
 
 use crate::core::err::ParseError;
-use crate::core::syntax::{Ast, AstKind, Token, TokenKind};
+use crate::core::syntax::{Ast, AstKind, Bp, Token, TokenKind};
 use crate::util::{Range, Scanner, Spot};
 use token_scanner::TokenScanner;
 
@@ -18,20 +18,68 @@ impl<'a> Parser<'a> {
         }
     }
 
-    pub fn parse(&self) -> ResAst {
+    pub fn parse(&mut self) -> ResAst {
         self.parse_program()
     }
 
-    fn parse_program(&self) -> ResAst {
+    fn parse_program(&mut self) -> ResAst {
+        self.parse_expression(Bp::get_lowest())
+    }
+
+    fn parse_expression(&mut self, threshold_bp: &Bp) -> ResAst {
+        let mut top = self.parse_expression_start()?;
+
+        loop {
+            let infix = match self.parse_expression_middle(&top, threshold_bp) {
+                Some(x) => x?,
+                None => {
+                    break;
+                }
+            };
+
+            top = infix;
+        }
+
+        Ok(top)
+    }
+
+    fn parse_expression_start(&mut self) -> ResAst {
         self.parse_num()
     }
 
-    fn parse_num(&self) -> ResAst {
+    fn parse_expression_middle(&mut self, left: &Ast, threshold_bp: &Bp) -> Option<ResAst> {
+        match self.scanner.read() {
+            Some(Token { kind, location }) => match kind {
+                TokenKind::Plus => {
+                    let bp = Bp::get_summative();
+                    if threshold_bp.right >= bp.left {
+                        return None;
+                    }
+
+                    self.scanner.advance();
+                    let right = match self.parse_expression(bp) {
+                        Ok(x) => x,
+                        x => return Some(x),
+                    };
+
+                    let parsed_ast = Ast::from_infix_plus(left.clone(), right);
+                    Some(Ok(parsed_ast))
+                }
+                _ => None,
+            },
+            _ => None,
+        }
+    }
+
+    fn parse_num(&mut self) -> ResAst {
         match self.scanner.read() {
             Some(Token {
                 kind: TokenKind::Number(n),
                 location,
-            }) => Ok(Ast::new(AstKind::Number(*n), *location)),
+            }) => {
+                self.scanner.advance();
+                Ok(Ast::new(AstKind::Number(*n), *location))
+            }
             _ => Err(ParseError::Unexpected(
                 "Unexpected".to_string(),
                 Range::new(Spot::new(0, 0), Spot::new(0, 0)),
@@ -70,5 +118,45 @@ mod tests {
         let expected = Ast::new(AstKind::Number(1.0), RANGE_MOCKS[0]);
         assert_eq!(ast, expected);
         Ok(())
+    }
+
+    mod infixes {
+        use super::*;
+
+        #[test]
+        fn test_parse_infix_plus() -> Res {
+            let tokens = vec![
+                Token::new(
+                    TokenKind::Number(1.0),
+                    Range::new(Spot::new(0, 0), Spot::new(1, 0)),
+                ),
+                Token::new(
+                    TokenKind::Plus,
+                    Range::new(Spot::new(1, 0), Spot::new(2, 0)),
+                ),
+                Token::new(
+                    TokenKind::Number(2.0),
+                    Range::new(Spot::new(2, 0), Spot::new(3, 0)),
+                ),
+            ];
+
+            let ast = parse(&tokens)?;
+
+            let expected = Ast::new(
+                AstKind::InfixPlus {
+                    left: Box::new(Ast::new(
+                        AstKind::Number(1.0),
+                        Range::new(Spot::new(0, 0), Spot::new(1, 0)),
+                    )),
+                    right: Box::new(Ast::new(
+                        AstKind::Number(2.0),
+                        Range::new(Spot::new(2, 0), Spot::new(3, 0)),
+                    )),
+                },
+                Range::new(Spot::new(0, 0), Spot::new(3, 0)),
+            );
+            assert_eq!(ast, expected);
+            Ok(())
+        }
     }
 }
