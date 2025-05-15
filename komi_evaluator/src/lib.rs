fde2d732761db6cb452d96ce499f6d574dd39f22
Cho <wcho.dev@gmail.com>
2025-05-15 19:42:45 +0000

    evaluate arithmetic expressions

--- komi_evaluator/src/lib.rs
+++ komi_evaluator/src/lib.rs
@@ -37,7 +37,6 @@ impl<'a> Evaluator<'a> {
             Ast { kind: AstKind::PrefixPlus { operand }, location } => Self::eval_prefix_plus(operand, location),
             Ast { kind: AstKind::PrefixMinus { operand }, location } => Self::eval_prefix_minus(operand, location),
             Ast { kind: AstKind::Number(n), location } => Self::eval_number(n, location),
-            _ => todo!(),
         }
     }
 
