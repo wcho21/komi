8f4722bf815d2e165905d6257c80e31f8a34960f
Cho <wcho.dev@gmail.com>
2025-05-15 19:59:38 +0000

    add integration tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -7,15 +7,45 @@ beforeAll(async () => {
 
 describe("ok", () => {
   it("single number literal", async () => {
-    const executed = execute("12.25");
+    const executed = execute("+12.25");
 
     expect(executed).toBe("komi v1 ok 12.25");
   });
 
-  it("addition", async () => {
-    const executed = execute("12 + 34.675");
+  it("addition, subtraction and mod", async () => {
+    const executed = execute("(12 % 5) + (34.25 - 30)");
 
-    expect(executed).toBe("komi v1 ok 46.675");
+    expect(executed).toBe("komi v1 ok 6.25");
+  });
+
+  it("subtraction and division", async () => {
+    const executed = execute("-1*2 * (3/4)");
+
+    expect(executed).toBe("komi v1 ok -1.5");
+  });
+
+  it("invalid number literal", async () => {
+    const executed = execute(".25");
+
+    expect(executed.startsWith("komi v1 err"));
+  });
+
+  it("invalid arithmetic expression", async () => {
+    const executed = execute("1 +");
+
+    expect(executed.startsWith("komi v1 err"));
+  });
+
+  it("no opening parenthesis", async () => {
+    const executed = execute("1 + 2)");
+
+    expect(executed.startsWith("komi v1 err"));
+  });
+
+  it("no closing parenthesis", async () => {
+    const executed = execute("(1 + 2");
+
+    expect(executed.startsWith("komi v1 err"));
   });
 });
 
