0a4472fb1769f95a87233a76720fb2c74c18d8df
Cho <wcho.dev@gmail.com>
2025-05-16 10:15:25 +0000

    add missing artifact

---
+++ dist/komi.d.ts
@@ -0,0 +1,4 @@
+/* tslint:disable */
+/* eslint-disable */
+export function execute(source: string): string;
+export function execute_with_stdout(source: string, format: string, write_stdout: Function): any;
