1f32a43871f7cfb0d8e943bb9b6d1b710f810fc7
Cho <wcho.dev@gmail.com>
2025-05-17 09:05:14 +0000

    remove unused build artifact

--- dist/komi.d.ts
+++
@@ -1,4 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export function execute(source: string): string;
-export function execute_with_stdout(source: string, format: string, write_stdout: Function): any;
--- dist/komi.js
+++
@@ -1,5 +0,0 @@
-import * as wasm from "./komi_bg.wasm";
-export * from "./komi_bg.js";
-import { __wbg_set_wasm } from "./komi_bg.js";
-__wbg_set_wasm(wasm);
-wasm.__wbindgen_start();
