f32dcd74d0c6f98d3898c4ef4b0ef89694dc2379
Cho <wcho.dev@gmail.com>
2025-05-24 09:33:45 +0000

    add shorthand method to abstract scanning step

--- komi_util/src/scanner/mod.rs
+++ komi_util/src/scanner/mod.rs
@@ -4,7 +4,20 @@ use crate::Range;
 pub trait Scanner {
     type Item;
 
+    // Returns the item at the current location.
     fn read(&self) -> Self::Item;
+
+    // Advances past the current item.
     fn advance(&mut self) -> ();
+
+    // Returns the location of the current item.
     fn locate(&self) -> Range;
+
+    // Returns the item at the current location, and advances.
+    // Same as calling `read()` and `locate()`.
+    fn read_and_advance(&mut self) -> Self::Item {
+        let item = self.read();
+        self.advance();
+        item
+    }
 }
