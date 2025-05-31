ffcc84db21329d382b8bc893f1df270699528b69
Cho <wcho.dev@gmail.com>
2025-05-31 10:57:36 +0000

    make wasm builder infer proper types

--- komi_wasm/src/lib.rs
+++ komi_wasm/src/lib.rs
@@ -1,11 +1,12 @@
 pub mod util;
 
-use util::res_converter::convert;
+use util::res_converter::JsConverter;
 use wasm_bindgen::prelude::*;
 
 pub type JsRes = Result<JsValue, JsValue>;
 
 #[wasm_bindgen]
 pub fn execute(source: &str) -> JsRes {
-    convert(&komi::execute(source))
+    let exec_out = komi::execute(source);
+    JsConverter::convert(exec_out)
 }
