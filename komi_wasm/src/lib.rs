052a2a22de0196c421d19a4a134a3373ae36dcb2
Cho <wcho.dev@gmail.com>
2025-05-31 11:53:06 +0000

    avoid to return js value for proper typing

--- komi_wasm/src/lib.rs
+++ komi_wasm/src/lib.rs
@@ -1,12 +1,12 @@
 pub mod util;
 
 use util::res_converter::JsConverter;
+pub use util::res_converter::{JsExecError, JsExecOut};
 use wasm_bindgen::prelude::*;
 
-pub type JsRes = Result<JsValue, JsValue>;
-
+/// Returns an object {@link ExecOut} if execution succeeds; throws an error {@link ExecError} if it fails.
 #[wasm_bindgen]
-pub fn execute(source: &str) -> JsRes {
+pub fn execute(source: &str) -> Result<JsExecOut, JsExecError> {
     let exec_out = komi::execute(source);
     JsConverter::convert(exec_out)
 }
--- komi_wasm/src/util/js_val/mod.rs
+++ komi_wasm/src/util/js_val/mod.rs
@@ -1,6 +1,5 @@
 pub mod obj;
 
-use crate::JsRes;
 use js_sys::{Error, Object};
 use komi_util::unpacker::unpack_spot;
 use komi_util::{Range, Spot};
@@ -30,7 +29,7 @@ pub fn convert_range_to_js_object(location: &Range) -> Result<Object, JsValue> {
 
 /// Converts an execution result to a JavaScript object with two fields `value` and `stdout`.
 /// These fields will be the string values from `repr` and `stdout`, respectively.
-pub fn convert_repr_and_stdout_to_js_val(repr: &str, stdout: &str) -> JsRes {
+pub fn convert_repr_and_stdout_to_js_val(repr: &str, stdout: &str) -> Result<JsValue, JsValue> {
     let obj = Object::new();
     obj::set_string_property(&obj, "value", repr)?;
     obj::set_string_property(&obj, "stdout", stdout)?;
@@ -38,7 +37,11 @@ pub fn convert_repr_and_stdout_to_js_val(repr: &str, stdout: &str) -> JsRes {
     Ok(obj.into())
 }
 
-pub fn convert_str_and_location_to_js_val(name: &str, message: &str, cause_location: &Range) -> JsRes {
+pub fn convert_str_and_location_to_js_val(
+    name: &str,
+    message: &str,
+    cause_location: &Range,
+) -> Result<JsValue, JsValue> {
     let cause = Object::new();
     let js_range_obj = convert_range_to_js_object(cause_location)?;
     obj::set_object_property(&cause, "location", &js_range_obj)?;
