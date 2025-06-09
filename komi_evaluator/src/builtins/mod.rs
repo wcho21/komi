2989b5e0cc3ee1bff885d2a9839d726cd1dce914
Cho <wcho.dev@gmail.com>
2025-06-09 00:16:45 +0000

    return error if without arguments

--- komi_evaluator/src/builtins/mod.rs
+++ komi_evaluator/src/builtins/mod.rs
@@ -1,5 +1,6 @@
 use crate::Environment as Env;
 use crate::ValRes;
+use komi_syntax::error::{EvalError, EvalErrorKind};
 use komi_syntax::value::{Stdout, Value, ValueKind};
 use komi_util::location::Range;
 
@@ -7,7 +8,11 @@ pub fn bind(env: &mut Env) -> () {
     env.set("쓰기", &Value::new(ValueKind::BuiltinFunc(stdout_write), Range::ORIGIN))
 }
 
-fn stdout_write(_location: &Range, args: &Vec<Value>, stdouts: &mut Stdout) -> ValRes {
+fn stdout_write(location: &Range, args: &Vec<Value>, stdouts: &mut Stdout) -> ValRes {
+    if args.len() == 0 {
+        return Err(EvalError::new(EvalErrorKind::BadNumArgs, *location));
+    }
+
     let strs: Vec<String> = args.iter().map(|arg| arg.represent()).collect();
     let joined = strs.join(" ");
     let joined_len = joined.chars().count();
